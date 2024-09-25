32342efdb28838449e13c31ed3d9ee8850f1b90f
Atr1ck <3126670240@qq.com>
2024-09-25 19:40:35 +0800

    commit

--- nonebot_lugin_pjsk_helper/__init__.py
+++ nonebot_lugin_pjsk_helper/__init__.py
@@ -3,7 +3,7 @@ from nonebot.plugin import PluginMetadata
 from .config import Config
 from . import pjsk as __main__
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __plugin_meta__ = PluginMetadata(
     name="pjsk-helper",
     description="基于 Nonebot2 的 Project Sekai 助手",
